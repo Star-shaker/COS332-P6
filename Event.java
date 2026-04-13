9b9d9d7241d1e514751b54f99f52ad1e8fd42105
Megan <u24575764@tuks.co.za>
2026-04-13 08:15:42 +0200

    Initial

---
+++ Event.java
@@ -0,0 +1,15 @@
+public class Event 
+{
+    public int day;
+    public int month;
+    public String description;
+
+    // dd/mm description
+
+    public Event(int day, int month, String description) 
+    {
+        this.day = day;
+        this.month = month;
+        this.description = description;
+    }    
+}
